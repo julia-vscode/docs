33b6f1476001ffe510e4d675c86e5ca9a8764246
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-18 01:07:22 +0000

    build based on ad76385

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Julia VS Code Extension Documentation",
+    "title": "Julia VS Code Extension Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Julia-VS-Code-Extension-Documentation-1",
+    "page": "Julia VS Code Extension Documentation",
+    "title": "Julia VS Code Extension Documentation",
+    "category": "section",
+    "text": ""
+},
+
+]}
