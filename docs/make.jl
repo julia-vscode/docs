511a6baa33784ffb71c20b41c9ff3f0fb0b384a7
David Anthoff <anthoff@berkeley.edu>
2020-04-19 13:25:44 +0200

    Add debugging section

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,7 @@ makedocs(
             "Understanding Code" => "userguide/understandingcode.md",
             "Linter" => "userguide/linter.md",
             "Tasks" => "userguide/tasks.md",
+            "Debugging" => "userguide/debugging.md",
             "Compiling Sysimages" => "userguide/compilesysimage.md",
             "Julia Markdown Documents" => "userguide/weave.md",
             "Remote Development" => "userguide/remote.md",
