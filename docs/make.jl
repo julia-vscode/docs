3e99d1e0fba92a5286a2b19eb5648585f8dfbc88
David Anthoff <anthoff@berkeley.edu>
2020-07-11 10:13:40 -0700

    Add v0.17 release notes to navigation

--- docs/make.jl
+++ docs/make.jl
@@ -24,6 +24,7 @@ makedocs(
             "Settings" => "userguide/settings.md"
         ],
         "Updates" => Any[
+            "Version v0.17" => "release-notes/v0_17.md",
             "Version v0.16" => "release-notes/v0_16.md"
         ],
         "Developer Documentation" => Any[
